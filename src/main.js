2c264f5fdd5d17ea180c9a6ebb2a97f4338617dd
Andre Madarang <andre_madarang@hotmail.com>
2018-08-02 11:52:37 -0400

    admin dashboard layout

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import locale from 'element-ui/lib/locale/lang/en'
+// import './assets/element-variables.scss'
+
+Vue.use(ElementUI, {locale});
 import App from './App.vue'
 
 Vue.config.productionTip = false
